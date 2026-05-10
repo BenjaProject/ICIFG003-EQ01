9832d53142ccca54ad18cf9213bc2618b04965e6
Benjisus3007 <benjamin.aedo3007@gmail.com>
2026-05-10 09:47:48 -0400

    Persistencia en landing

--- frontend/src/app/features/users/components/landing-component/landing-component.ts
+++ frontend/src/app/features/users/components/landing-component/landing-component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from "@angular/router";
+import { UsuarioStore } from '../../services/usuario-store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-component',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './landing-component.html',
   styleUrl: './landing-component.css',
 })
-export class LandingComponent {}
+export class LandingComponent {
+
+  readonly usuarioStore = inject(UsuarioStore);
+  private router = inject(Router);
+
+  logout(): void {
+
+    this.usuarioStore.logout();
+    this.router.navigate(['/']);
+
+  }
+
+}
